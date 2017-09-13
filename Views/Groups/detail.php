ffa9298f81f807797c79e7b58f32270943638e91
Soejima <hand36ideal@yahoo.co.jp>
2017-09-13 21:54:28 +0900

    グループ詳細ページ仕上げ

--- Views/Groups/detail.php
+++ Views/Groups/detail.php
@@ -1,1 +1,32 @@
-<h2><?= $data['group']['group_name'] ?></h2>
+<h2><?= $data['group']['group_name'] ?><a href="" class="btn right btn-success">メンバー追加</a></h2>
+<table class="table table-striped groups">
+  <thead>
+      <tr>
+          <th>ID</th>
+          <th>名前</th>
+          <th>経路</th>
+          <th>アドレス</th>
+          <th></th>
+      </tr>
+  </thead>
+  <tbody>
+
+<?php if (count($data['records']) > 0) { ?>
+  <?php foreach ($data['records'] as $row) { ?>
+    <tr>
+      <!--  'member_id', m.name AS 'name', mr.name AS 'line', m.mail AS 'mail'  -->
+      <td><?=$row['member_id']?></td>
+      <td><?=$row['name']?></td>
+      <td><?=$row['line']?></td>
+      <td><?=$row['mail']?></td>
+      <td></td>
+    </tr>
+  <?php } ?>
+<?php } else { ?>
+    <tr>
+        <td colspan="5">グループがありません</td>
+    </tr>
+<?php } ?>
+  </tbody>
+
+</table>
