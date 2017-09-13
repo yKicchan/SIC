0297435f923e08e69bf4ea975f6705cd67cd65e5
yKicchan <yKicchanApp@gmail.com>
2017-09-14 01:24:04 +0900

    clean[#0]: ライブラリの読み込み先変更

--- Views/header.php
+++ Views/header.php
@@ -2,10 +2,12 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="/lib/bootstrap-3.3.7-dist/css/bootstrap.min.css">
-        <link rel="stylesheet" type="text/css" href="/lib/bootstrap-xxs-1.0.2.css">
-        <link rel="stylesheet" href="/css/master.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="/css/master.css">
         <title><?= $data['title'] ?></title>
     </head>
     <body class="container">
